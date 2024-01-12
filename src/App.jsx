e125e99a9754ba955580eba41c86343652d5a07c
Jansen Muniz <jansen.muniz1@gmail.com>
2024-01-12 17:36:22 -0300

    primeiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+const App = () => {
+  return (
+    <div className="main-container">
+      <header>
+        <div className="logo-container">
+          <h1>logo</h1>
+        </div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Sobre</li>
+            <li>Páginas</li>
+            <li>Contato</li>
+          </ul>
+        </nav>
+        <div>
+          <button>
+            <span>Comprar ingresso</span>
+          </button>
+        </div>
+      </header>
+      <main>
+        <h1>Bem-vindo</h1>
+        <div>
+          <img src="public/img/undraw_Happy_announcement_re_tsm0.png" alt="" />
+        </div>
+      </main>
+    </div>
+  )
+}
 
 export { App }
